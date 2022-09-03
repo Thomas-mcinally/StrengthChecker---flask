9d4daf4ce87ddb8ce9658c7d1f3a4d2343de0713
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-09-03 15:41:56 +0100

    add formatting tools

--- init_db.py
+++ init_db.py
@@ -1,10 +1,11 @@
-import csv,sqlite3
+import csv, sqlite3
 
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect("database.db")
 cur = connection.cursor()
 
-#create male_lifters table
-cur.execute('''CREATE TABLE male_lifters(sex TEXT
+# create male_lifters table
+cur.execute(
+    """CREATE TABLE male_lifters(sex TEXT
                                         ,event TEXT
                                         ,equipment TEXT
                                         ,age FLOAT
@@ -14,10 +15,12 @@ cur.execute('''CREATE TABLE male_lifters(sex TEXT
                                         ,best3DeadliftKg FLOAT
                                         ,totalKg FLOAT
                                         ,age_bin TEXT
-                                        ,weight_bin TEXT)''')
+                                        ,weight_bin TEXT)"""
+)
 
-#create female_lifters table
-cur.execute('''CREATE TABLE female_lifters(sex TEXT
+# create female_lifters table
+cur.execute(
+    """CREATE TABLE female_lifters(sex TEXT
                                           ,event TEXT
                                           ,equipment TEXT
                                           ,age FLOAT
@@ -27,29 +30,61 @@ cur.execute('''CREATE TABLE female_lifters(sex TEXT
                                           ,best3DeadliftKg FLOAT
                                           ,totalKg FLOAT
                                           ,age_bin TEXT
-                                          ,weight_bin TEXT)''')
+                                          ,weight_bin TEXT)"""
+)
 
-#fill male_lifters table
-with open('male_powerlifters.csv','r') as fin: 
+# fill male_lifters table
+with open("male_powerlifters.csv", "r") as fin:
     # csv.DictReader uses first line in file for column headings by default
-    dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['sex'], i['event'], i['equipment'], i['age'], i['bodyweightKg'], i['best3SquatKg'], i['best3BenchKg'], i['best3DeadliftKg'], i['totalKg'], i['age_bin'], i['weight_bin']) for i in dr]
+    dr = csv.DictReader(fin)  # comma is default delimiter
+    to_db = [
+        (
+            i["sex"],
+            i["event"],
+            i["equipment"],
+            i["age"],
+            i["bodyweightKg"],
+            i["best3SquatKg"],
+            i["best3BenchKg"],
+            i["best3DeadliftKg"],
+            i["totalKg"],
+            i["age_bin"],
+            i["weight_bin"],
+        )
+        for i in dr
+    ]
 
-cur.executemany("INSERT INTO male_lifters (sex,event,equipment,age,bodyweightKg,best3SquatKg,best3BenchKg,best3DeadliftKg,totalKg,age_bin,weight_bin) VALUES (?,?,?,?,?,?,?,?,?,?,?);", to_db)
+cur.executemany(
+    "INSERT INTO male_lifters (sex,event,equipment,age,bodyweightKg,best3SquatKg,best3BenchKg,best3DeadliftKg,totalKg,age_bin,weight_bin) VALUES (?,?,?,?,?,?,?,?,?,?,?);",
+    to_db,
+)
 
-#fill female_lifters table
-with open('female_powerlifters.csv','r') as fin: 
+# fill female_lifters table
+with open("female_powerlifters.csv", "r") as fin:
     # csv.DictReader uses first line in file for column headings by default
-    dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['sex'], i['event'], i['equipment'], i['age'], i['bodyweightKg'], i['best3SquatKg'], i['best3BenchKg'], i['best3DeadliftKg'], i['totalKg'], i['age_bin'], i['weight_bin']) for i in dr]
+    dr = csv.DictReader(fin)  # comma is default delimiter
+    to_db = [
+        (
+            i["sex"],
+            i["event"],
+            i["equipment"],
+            i["age"],
+            i["bodyweightKg"],
+            i["best3SquatKg"],
+            i["best3BenchKg"],
+            i["best3DeadliftKg"],
+            i["totalKg"],
+            i["age_bin"],
+            i["weight_bin"],
+        )
+        for i in dr
+    ]
 
-cur.executemany("INSERT INTO female_lifters (sex,event,equipment,age,bodyweightKg,best3SquatKg,best3BenchKg,best3DeadliftKg,totalKg,age_bin,weight_bin) VALUES (?,?,?,?,?,?,?,?,?,?,?);", to_db)
+cur.executemany(
+    "INSERT INTO female_lifters (sex,event,equipment,age,bodyweightKg,best3SquatKg,best3BenchKg,best3DeadliftKg,totalKg,age_bin,weight_bin) VALUES (?,?,?,?,?,?,?,?,?,?,?);",
+    to_db,
+)
 
-#important to commit and close for new rows to be added to tables
+# important to commit and close for new rows to be added to tables
 connection.commit()
 connection.close()
-
-
-
-
-
